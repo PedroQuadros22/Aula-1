fea66212dbd02d18a7c34b3c9d7426b1e7a03a16
PedroQuadros22 <121057915+PedroQuadros22@users.noreply.github.com>
2023-09-11 19:45:57 -0300

    Add files via upload

---
+++ lista 1/test2.py
@@ -0,0 +1,14 @@
+print('4x**2-4x-24')
+a=4
+b=-4
+c=-24
+print('a=',a,'b=', b,'c=',c)
+delta=b**2-4*a*c
+print('delta = b**2-4*a*c')
+print('delta=', delta)
+print('x1=(-b+delta**(1/2))/(2*a)')
+x1=(-b+delta**(1/2))/(2*a)
+print('x1=',x1)
+print('x2=(-b-delta**(1/2))/(2*a)')
+x2=(-b-delta**(1/2))/(2*a)
+print('x2=',x2)
