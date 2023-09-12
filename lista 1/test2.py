8020d027ff56631c2ec81cf093d07741fb974e24
ArthusAlmeida <144635349+ArthusAlmeida@users.noreply.github.com>
2023-09-12 17:04:48 -0600

    calculadora

--- lista 1/test2.py
+++ lista 1/test2.py
@@ -7,6 +7,14 @@ delta=b**2-4*a*c
 print('delta = b²-4*a*c')
 print('delta = ({})²-4*({})*({})'.format(b,a,c))
 print('delta=', delta)
+if delta<0:
+    print("Delta não possui valor Real")
+if delta==0:
+    print("A equação só possui um unico valor Real")
+    print('x1=(-b/(2*a)')
+    print('x1=(-({})/(2*{})'.format(b,a))
+    x1=-b/(2*a)
+    print('x1=',x1)
 print('x1=(-b+delta**(1/2)/(2*a)')
 print('x1=(-({})+{}**(1/2)/(2*{})'.format(b,delta,a))
 x1=(-b+delta**(1/2))/(2*a)
@@ -15,5 +23,3 @@ print('x2=(-b-delta**(1/2))/(2*a)')
 print('x2=(-({})-{}**(1/2)/(2*{})'.format(b,delta,a))
 x2=(-b-delta**(1/2))/(2*a)
 print('x2=',x2)
-
-
