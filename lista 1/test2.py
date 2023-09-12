fe6af841121529648ea0a49f5bd515bde3845637
ArthusAlmeida <144635349+ArthusAlmeida@users.noreply.github.com>
2023-09-12 16:32:15 -0600

    mudancasa no test 2

--- lista 1/test2.py
+++ lista 1/test2.py
@@ -1,14 +1,16 @@
-print('4x**2-4x-24')
-a=4
-b=-4
-c=-24
-print('a=',a,'b=', b,'c=',c)
+print('a.x²=b.x+c')
+a=int(input('Escolha o valor para a: '))
+b=int(input('Escolha o valor para b: '))
+c=int(input('Escolha o valor para c: '))
+print('{}.x²+({}).x+({})'.format(a,b,c))
 delta=b**2-4*a*c
-print('delta = b**2-4*a*c')
+print('delta = b²-4*a*c')
+print('delta = ({})²-4*({})*({})'.format(b,a,c))
 print('delta=', delta)
-print('x1=(-b+delta**(1/2))/(2*a)')
+print('x1=(-b+delta**(1/2)/(2*a)')
 x1=(-b+delta**(1/2))/(2*a)
 print('x1=',x1)
 print('x2=(-b-delta**(1/2))/(2*a)')
 x2=(-b-delta**(1/2))/(2*a)
 print('x2=',x2)
+
