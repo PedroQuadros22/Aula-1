d49f14c4ef8c0e0892f5afbc938f311b5506dcb5
ArthusAlmeida <144635349+ArthusAlmeida@users.noreply.github.com>
2023-09-12 16:43:25 -0600

    fim

--- lista 1/test2.py
+++ lista 1/test2.py
@@ -8,10 +8,12 @@ print('delta = b²-4*a*c')
 print('delta = ({})²-4*({})*({})'.format(b,a,c))
 print('delta=', delta)
 print('x1=(-b+delta**(1/2)/(2*a)')
+print('x1=(-({})+{}**(1/2)/(2*{})'.format(b,delta,a))
 x1=(-b+delta**(1/2))/(2*a)
 print('x1=',x1)
 print('x2=(-b-delta**(1/2))/(2*a)')
+print('x2=(-({})-{}**(1/2)/(2*{})'.format(b,delta,a))
 x2=(-b-delta**(1/2))/(2*a)
 print('x2=',x2)
-print("ola")
+
 
